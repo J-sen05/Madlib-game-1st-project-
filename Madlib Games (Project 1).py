984b4abcad2eb11e8f5dda079959af879ac036af
J-sen05 <elsenjimn05@gmail.com>
2026-02-09 15:52:10 +0800

    Add files via upload

---
+++ Madlib Games (Project 1).py
@@ -0,0 +1,16 @@
+#Madlib game
+name = input("Enter a pet name:")
+food = input("Enter a food name:")
+place1 = input("Enter a place:")
+action = input("Enter an action with an 'ed':")
+someone = input("Enter a person:")
+
+
+print (f"\nI have a dog, her name is {name}.")
+print (f"{name} likes to eat {food}, it's her favourite")
+print (f"{name} is the only one that makes me happy in life.")
+print (f"One day, when i got back from {place1}")
+print (f"I ran straight to {name} but shes unresponsive, so I {action} and called for {someone}")
+print (f"{someone} help me drive {name} to a veterinarian.")
+print (f"But to no avail, it was too late.")
+print (f"{name} already cross the rainbow bridge, leaving me behind")
